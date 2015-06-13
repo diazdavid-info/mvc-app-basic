1c34447cb004a3e0a8e67648c9106b0fd1ae9c0c
David Díaz <diazdavid.info@gmail.com>
2015-06-13 18:31:29 +0200

    commit inicial

---
+++ tests/MvcAppBasicTest.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: daviddiaz
+ * Date: 7/6/15
+ * Time: 14:21
+ */
+
+namespace Test;
+
+use Controller\FrontController;
+
+class MvcAppBasicTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testNothingTest()
+    {
+        $this->assertTrue(true);
+    }
+}
